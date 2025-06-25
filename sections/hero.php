c1e15c0adf49f6cc56ae14c3ae765836adc05360
EdgarRCarrera <89110483+EdgarRCarrera@users.noreply.github.com>
2025-06-25 05:52:32 -0600

    Add modular hero and auto-hide header

---
+++ sections/hero.php
@@ -0,0 +1,9 @@
+<section class="hero" style="background-image:url('img/hero-doctors.jpg');">
+  <div class="hero__content">
+    <h1 class="hero__title">Avoid Hassles & Delays.</h1>
+    <p class="hero__text">How is health today? Sounds like not good! Don't worry. Find your doctor online and book as you wish with eDoc.</p>
+    <p class="hero__text">We offer you a free doctor channeling service. Make your appointment now.</p>
+    <a href="/patient/" class="hero__btn" aria-label="Book an appointment">Make Appointment</a>
+  </div>
+</section>
+
